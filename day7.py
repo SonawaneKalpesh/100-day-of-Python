733cb3fdd5e85689104a0a764193e904c5ded1e2
SonawaneKalpesh <kalpeshsonawane990@gmail.com>
2026-02-08 21:23:41 +0530

    Find the larger of two numbers

---
+++ day7.py
@@ -0,0 +1,9 @@
+a=int(input("enter a first number"))
+b=int(input("enter secound number"))
+
+if a>b:
+    print(a,"is larger than ",b)
+elif a==b:
+    print(a,"is equal to ",b)
+else:
+    print(a,"is smaller than ",b)
