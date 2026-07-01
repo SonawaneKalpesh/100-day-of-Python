4d5d3749152de8d3dbc7542103621a2f52446158
SonawaneKalpesh <kalpeshsonawane990@gmail.com>
2026-07-01 07:31:33 +0530

    Character frequency counter

---
+++ day34.py
@@ -0,0 +1,13 @@
+text = input("Enter a string: ").lower()
+
+frequency = {}
+
+for char in text:
+    if char in frequency:
+        frequency[char] += 1
+    else:
+        frequency[char] = 1
+
+print("Character Frequency:")
+for char, count in frequency.items():
+    print(f"{char}: {count}")
