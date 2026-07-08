aa5cd1dfbb74396567b2ede9c87f9e2ffe79d6e8
SonawaneKalpesh <kalpeshsonawane990@gmail.com>
2026-07-08 23:34:06 +0530

    Find max and min in a list

---
+++ day41.py
@@ -0,0 +1,14 @@
+numbers = list(map(int, input("Enter numbers separated by space: ").split()))
+
+maximum = numbers[0]
+minimum = numbers[0]
+
+for num in numbers:
+    if num > maximum:
+        maximum = num
+
+    if num < minimum:
+        minimum = num
+
+print("Maximum:", maximum)
+print("Minimum:", minimum)
