51fe9a4a400f16194ce26237bbc75f7848741c59
SonawaneKalpesh <kalpeshsonawane990@gmail.com>
2026-07-18 09:17:14 +0530

    Flatten a nested list

---
+++ day51.py
@@ -0,0 +1,9 @@
+nested_list = [[1, 2], [3, 4], [5, 6]]
+
+flat_list = []
+
+for sublist in nested_list:
+    for item in sublist:
+        flat_list.append(item)
+
+print("Flattened list:", flat_list)
