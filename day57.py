3b407fc193fccc7c7b1e06fb30202a2ee6a6a7f1
SonawaneKalpesh <kalpeshsonawane990@gmail.com>
2026-07-30 21:44:19 +0530

    Word frequency counter

---
+++ day57.py
@@ -0,0 +1,16 @@
+sentence = input("Enter a sentence: ").lower()
+
+words = sentence.split()
+
+frequency = {}
+
+for word in words:
+    if word in frequency:
+        frequency[word] += 1
+    else:
+        frequency[word] = 1
+
+print("\nWord Frequency:")
+
+for word, count in frequency.items():
+    print(f"{word}: {count}")
