84152393629f960c753bc693b18c60a5f2f86340
SonawaneKalpesh <kalpeshsonawane990@gmail.com>
2026-08-30 21:24:39 +0530

    Method overriding

---
+++ day86.py
@@ -0,0 +1,22 @@
+class Animal:
+    def sound(self):
+        print("Animal makes a sound")
+
+
+class Dog(Animal):
+    def sound(self):
+        print("Dog barks")
+
+
+class Cat(Animal):
+    def sound(self):
+        print("Cat meows")
+
+
+animal = Animal()
+dog = Dog()
+cat = Cat()
+
+animal.sound()
+dog.sound()
+cat.sound()
